f7b58f8163de9b5f4e62e7a9338e3b8d71be90f2
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-03-28 16:08:28 +0200

    Setting the Toggle Button

--- client/src/Components/navbar.jsx
+++ client/src/Components/navbar.jsx
@@ -1,24 +1,31 @@
-import { Button, Flex, Spacer, Icon, Text, useDisclosure } from '@chakra-ui/react';
+import {
+  Button,
+  Flex,
+  Spacer,
+  Icon,
+  Text,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from '@chakra-ui/react';
 import { Link, Link as ReactLink } from 'react-router-dom';
 import { BsNewspaper } from 'react-icons/bs';
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import changeView from '../Redux/Actions/viewActions';
 
 const Navbar = () => {
-  //Toggle Views using React State Hooks
-  // const [tileView, setView] = useState(true);
-
-  //Toggle Views using Redux
-  const isGridView = useSelector((state) => state.view.isGridView);
+  const { isOpen, onOpen, onClose } = useDisclosure(); // For the toggle button
+  const isGridView = useSelector((state) => state.view.isGridView); //For the view button
   const dispatch = useDispatch();
   const setView = () => {
     dispatch(changeView());
   };
-  //=======================
-  const { getDisclosureProps, getButtonProps } = useDisclosure();
-  const buttonProps = getButtonProps();
-  const disclosureProps = getDisclosureProps();
+
   return (
     <Flex align='center' bg='gray.800' color='white' height='60px' px='20px'>
       <Link as={ReactLink} to='/'>
@@ -28,20 +35,30 @@ const Navbar = () => {
         </Flex>
       </Link>
       <Spacer />
-      {/* <Button onClick={() => setView(!tileView)} size='sm' colorScheme='blue'>
-        {tileView === true ? 'Switch to Tiles' : 'Switch to List'}
-      </Button> */}
-      <Button onClick={setView} size='sm' colorScheme='blue'>
+      <Button onClick={setView} size='sm' colorScheme='blue' margin={'1px'}>
         {isGridView ? 'List View' : 'Grid View'}
       </Button>
-      <Button {...buttonProps} size='sm' colorScheme='red'>
+      <Button onClick={onOpen} size='sm' colorScheme='red' margin={'1px'}>
         Toggle Me
       </Button>
-      <Text {...disclosureProps} mt={4}>
-        The Hardest Par for me is the redux 😄
-        <br />
-        The best part about the Project is everything in it 💌
-      </Text>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Hi 😃</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>For me the hardest part about the project is setting the Redux store.</Text>
+            <Text>
+              However the Redux is still 😆 the best part as it's my first time to fully implemtn it's idea from A to Z.
+            </Text>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 };
